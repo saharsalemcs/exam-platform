20368f246f45d5dafd6ddeb164d78e1475695104
saharsalemcs <saharsalemcs@gmail.com>
2026-08-04 19:23:18 +0300

    style(NotFoundPage): enhance layout and replace button with shared Button and finish the page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,20 +1,23 @@
+import Button from "@/components/shared/Button";
 import { useNavigate } from "react-router-dom";
 
 function NotFoundPage() {
   const navigate = useNavigate();
 
   return (
-    <div className="flex min-h-screen animate-fade-up flex-col items-center justify-center bg-bg p-6">
+    <div className="flex min-h-screen animate-fade-up flex-col items-center justify-center bg-bg p-6 text-center">
       <h1 className="mb-4 text-6xl font-bold tracking-tight text-text">
         ERROR 404
       </h1>
       <h3 className="mb-6 text-3xl font-semibold text-text">Page Not Found</h3>
 
-      <p className="mb-10 text-base text-text-muted md:text-lg">
+      <p className="mx-auto mb-10 max-w-5xl text-base text-text-muted sm:text-lg md:text-lg">
         The page you're looking for doesn't exist, or was moved.
       </p>
 
-      <button onClick={() => navigate(-1)}>Go Back</button>
+      <Button size="lg" onClick={() => navigate(-1)}>
+        Go Back
+      </Button>
     </div>
   );
 }
